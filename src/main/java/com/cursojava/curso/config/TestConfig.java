76ef7c5f8aca445eeacb0e9cf12bc6512425110b
Gabriel <thebug@outlook.com.br>
2026-06-16 16:15:27 -0300

    Adicionado enumerador de status no pedido(order)

--- src/main/java/com/cursojava/curso/config/TestConfig.java
+++ src/main/java/com/cursojava/curso/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.cursojava.curso.entities.Order;
 import com.cursojava.curso.entities.User;
+import com.cursojava.curso.entities.enums.OrderStatus;
 import com.cursojava.curso.repositories.OrderRepository;
 import com.cursojava.curso.repositories.UserRepository;
 
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-06-21T19:53:07Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-06-22T19:53:07Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-06-21T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-06-22T19:53:07Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
