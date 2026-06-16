9c5e565f1e8cda0bbaded7870145613734527413
Gabriel <thebug@outlook.com.br>
2026-06-16 11:34:09 -0300

    Adicionado registro de componentes e endpoint para buscar um usuario por id.

--- src/main/java/com/cursojava/curso/resources/UserResource.java
+++ src/main/java/com/cursojava/curso/resources/UserResource.java
@@ -1,20 +1,33 @@
 package com.cursojava.curso.resources;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursojava.curso.entities.User;
+import com.cursojava.curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	@Autowired
+	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Optional<User>> findById(@PathVariable long id){
+		return ResponseEntity.ok().body(service.findById(id));
 	}
 	
 }
---
+++ src/main/java/com/cursojava/curso/services/UserService.java
@@ -0,0 +1,26 @@
+package com.cursojava.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cursojava.curso.entities.User;
+import com.cursojava.curso.repositories.UserRepository;
+
+@Service
+public class UserService {
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public Optional<User> findById(long id) {
+		return repository.findById(id);
+	}
+}
+
+
