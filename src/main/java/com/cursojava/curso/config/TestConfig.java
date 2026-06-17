017ecbe55cf8957d5379a5037ccf6a08f21c3b68
Gabriel <thebug@outlook.com.br>
2026-06-17 15:53:58 -0300

    Finalizado relacionamento entre item do pedido(OrderItem) e pedido(Order)

--- src/main/java/com/cursojava/curso/config/TestConfig.java
+++ src/main/java/com/cursojava/curso/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.cursojava.curso.entities.Category;
 import com.cursojava.curso.entities.Order;
+import com.cursojava.curso.entities.OrderItem;
 import com.cursojava.curso.entities.Product;
 import com.cursojava.curso.entities.User;
 import com.cursojava.curso.entities.enums.OrderStatus;
 import com.cursojava.curso.repositories.CategoryRepository;
+import com.cursojava.curso.repositories.OrderItemRepository;
 import com.cursojava.curso.repositories.OrderRepository;
 import com.cursojava.curso.repositories.ProductRepository;
 import com.cursojava.curso.repositories.UserRepository;
@@ -30,6 +32,8 @@ public class TestConfig implements CommandLineRunner {
 	private CategoryRepository categoryRepository;
 	@Autowired
 	private ProductRepository productRepository;
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,6 +72,13 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(c2);
 		
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p4.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 	}
 	
